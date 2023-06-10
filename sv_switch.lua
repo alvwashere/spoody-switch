65159cd0485708c7f3c278900f6ee19439cf319a
Alv#9999 <72032190+alvwashere@users.noreply.github.com>
2023-06-10 02:59:09 +0100

    Update sv_switch.lua
    Added sv_side for different switch types

--- sv_switch.lua
+++ sv_switch.lua
@@ -1,6 +1,22 @@
-ESX.RegisterUsableItem('switch', function(source)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    xPlayer.removeInventoryItem('switch', 1) --This removes the item from the players inventory
+--[[
 
-    TriggerClientEvent("spoody:switch", source)
-end)
+New Update:
+
+Added SwitchTypes table which will be passed to the client so you can provide different stats for different switches
+For example, I have added switch which is a default switch and better_switch which will give better stats pepega
+Completely expermenting with this not sure if i'll even finish it 
+
+]]
+
+SwitchTypes={
+    'switch',
+    'better_switch'
+}
+
+for k, v in pairs(SwitchTypes) do
+    ESX.RegisterUsableItem(v, function(source)
+        xPlayer=ESX.GetPlayerFromId(source)
+            
+        TriggerClientEvent('spoody_switch', source, v)
+    end)
+end
