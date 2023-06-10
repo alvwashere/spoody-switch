b33fa0fc0a3c7bb4ddef0effa57233ba23357be9
Alv#9999 <72032190+alvwashere@users.noreply.github.com>
2023-06-10 02:54:27 +0100

    Update sv_switch.lua
    Remove export for ESX since it is being called through the manifest now

--- sv_switch.lua
+++ sv_switch.lua
@@ -1,10 +1,3 @@
-Legacy = true
-
-if not Legacy then
-    ESX = exports['es_extended']:getSharedObject()
-end
-
-
 ESX.RegisterUsableItem('switch', function(source)
     local xPlayer = ESX.GetPlayerFromId(source)
     xPlayer.removeInventoryItem('switch', 1) --This removes the item from the players inventory
