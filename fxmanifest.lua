c6ae355ad2bc5395721927f7c99af6a6d149f8ee
Alv#9999 <72032190+alvwashere@users.noreply.github.com>
2023-06-10 02:53:10 +0100

    Update manifest
    Not really an important change just put fx_version to latest and added ESX through imports since its quicker.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,10 @@
-fx_version 'bodacious'
-
+fx_version 'cerulean'
 author 'Spoody'
+description 'Spoody-Switches improved by Alv.gg'
 
 game 'gta5'
+lua54 'yes'
 
+shared_script '@es_extended/imports.lua'
 client_script 'client.lua'
-
 server_script 'server.lua'
